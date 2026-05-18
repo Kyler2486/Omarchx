d3ccb771f536d98c0753ced1b630dabcb4d8525c
Kyler <155092008+Kyler2486@users.noreply.github.com>
2026-05-18 18:03:35 -0400

    Update mirror URLs in boot.sh script

--- boot.sh
+++ boot.sh
@@ -24,13 +24,13 @@ OMARCHX_REF="${OMARCHX_REF:-dev}"
 # Set mirror based on branch
 if [[ $OMARCHX_REF == "dev" ]]; then
   export OMARCHX_MIRROR=edge
-  echo 'Server = https://mirror.omarchx.org/$repo/os/$arch' | sudo tee /etc/pacman.d/mirrorlist >/dev/null
+  echo 'Server = https://mirror.omarchy.org/$repo/os/$arch' | sudo tee /etc/pacman.d/mirrorlist >/dev/null
 elif [[ $OMARCHX_REF == "rc" ]]; then
   export OMARCHX_MIRROR=rc
-  echo 'Server = https://rc-mirror.omarchx.org/$repo/os/$arch' | sudo tee /etc/pacman.d/mirrorlist >/dev/null
+  echo 'Server = https://rc-mirror.omarchy.org/$repo/os/$arch' | sudo tee /etc/pacman.d/mirrorlist >/dev/null
 else
   export OMARCHX_MIRROR=stable
-  echo 'Server = https://stable-mirror.omarchx.org/$repo/os/$arch' | sudo tee /etc/pacman.d/mirrorlist >/dev/null
+  echo 'Server = https://stable-mirror.omarchy.org/$repo/os/$arch' | sudo tee /etc/pacman.d/mirrorlist >/dev/null
 fi
 
 sudo pacman -Syu --noconfirm --needed git
