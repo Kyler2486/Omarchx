2ebcd44b9a4b515b6325cca4bd04ca1d374b538f
Kyler <155092008+Kyler2486@users.noreply.github.com>
2026-05-18 16:53:07 -0400

    Update installation script for Omarchx

--- install.sh
+++ install.sh
@@ -4,15 +4,15 @@
 set -eEo pipefail
 
 # Define Omarchy locations
-export OMARCHY_PATH="$HOME/.local/share/omarchy"
-export OMARCHY_INSTALL="$OMARCHY_PATH/install"
-export OMARCHY_INSTALL_LOG_FILE="/var/log/omarchy-install.log"
-export PATH="$OMARCHY_PATH/bin:$PATH"
+export OMARCHX_PATH="$HOME/.local/share/omarchx"
+export OMARCHX_INSTALL="$OMARCHX_PATH/install"
+export OMARCHX_INSTALL_LOG_FILE="/var/log/omarchx-install.log"
+export PATH="$OMARCHX_PATH/bin:$PATH"
 
 # Install
-source "$OMARCHY_INSTALL/helpers/all.sh"
-source "$OMARCHY_INSTALL/preflight/all.sh"
-source "$OMARCHY_INSTALL/packaging/all.sh"
-source "$OMARCHY_INSTALL/config/all.sh"
-source "$OMARCHY_INSTALL/login/all.sh"
-source "$OMARCHY_INSTALL/post-install/all.sh"
+source "$OMARCHX_INSTALL/helpers/all.sh"
+source "$OMARCHX_INSTALL/preflight/all.sh"
+source "$OMARCHX_INSTALL/packaging/all.sh"
+source "$OMARCHX_INSTALL/config/all.sh"
+source "$OMARCHX_INSTALL/login/all.sh"
+source "$OMARCHX_INSTALL/post-install/all.sh"
