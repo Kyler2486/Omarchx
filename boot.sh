008ada19bd64b1cbeca6642a1e8e19aac808f901
Kyler <155092008+Kyler2486@users.noreply.github.com>
2026-05-20 16:34:20 -0400

    Refactor boot.sh for better installation handling
    Updated boot script to improve installation process and user feedback.

--- boot.sh
+++ boot.sh
@@ -1,51 +1,56 @@
-#!/bin/bash
-
-# Set install mode to online since boot.sh is used for curl installations
-export OMARCHX_ONLINE_INSTALL=true
+#!/usr/bin/env bash
+export PATH=/usr/bin:/bin:/usr/sbin:/sbin:$PATH
 
 ansi_art='
 ▄█████▄    ▄███████████▄    ▄███████   ▄███████   ▄███████   ▄█   █▄    ▄█   █▄
 ███   ███  ███   ███   ███  ███   ███  ███   ███  ███   ███  ███   ███  ███   ███
 ███   ███  ███   ███   ███  ███   ███  ███   ███  ███   █▀   ███   ███   ▀███▄██▀
-███   ███  ███   ███   ███ ▄███▄▄▄███ ▄███▄▄▄██▀  ███       ▄███▄▄▄███▄   █████ 
+███   ███  ███   ███   ███ ▄███▄▄▄███ ▄███▄▄▄██▀  ███       ▄███▄▄▄███▄   █████
 ███   ███  ███   ███   ███ ▀███▀▀▀███ ▀███▀▀▀▀    ███      ▀▀███▀▀▀███   ▄██▀██▄
 ███   ███  ███   ███   ███  ███   ███ ██████████  ███   █▄   ███   ███  ███   ███
 ███   ███  ███   ███   ███  ███   ███  ███   ███  ███   ███  ███   ███  ███   ███
- ▀█████▀    ▀█   ███   █▀   ███   █▀   ███   ███  ███████▀   ███   █▀   ▀█   █▀ 
+ ▀█████▀    ▀█   ███   █▀   ███   █▀   ███   ███  ███████▀   ███   █▀   ▀█   █▀
                                        ███   █▀
 '
 
 clear
-echo -e "\n$ansi_art\n"
+echo -e "\n$ansi_art\n\nLet's install Omarchx!\n"
+sleep 1
+printf "Installing/updating git.\n"
+sleep 1
 
-# Use custom branch if instructed, otherwise default to master
-OMARCHX_REF="${OMARCHX_REF:-dev}"
+pkg=git
 
-# Set mirror based on branch
-if [[ $OMARCHX_REF == "dev" ]]; then
-  export OMARCHX_MIRROR=edge
-  echo 'Server = https://geo.mirror.pkgbuild.com/$repo/os/$arch' | tee /etc/pacman.d/mirrorlist >/dev/null
-elif [[ $OMARCHX_REF == "rc" ]]; then
-  export OMARCHX_MIRROR=rc
-  echo 'Server = https://geo.mirror.pkgbuild.com/$repo/os/$arch' | tee /etc/pacman.d/mirrorlist >/dev/null
+  install_pkg() {
+  pacman -Syu --noconfirm --needed "$pkg" --quiet >/dev/null 2>&1
+}
+
+dots_spinner() {
+  local pid=$1 delay=0.4 n=0 max=3 dots='...'
+  tput civis 2>/dev/null
+  while kill -0 "$pid" 2>/dev/null; do
+    n=$(( (n % max) + 1 ))
+    printf "\r%.*s\033[K" "$n" "$dots"
+    sleep "$delay"
+  done
+  tput cnorm 2>/dev/null
+  printf "\r\033[K"
+}
+
+install_pkg & pid=$!
+dots_spinner "$pid"
+
+wait "$pid"
+status=$?
+if [ "$status" -eq 0 ]; then
+  printf "Installed %s\n" "$pkg"
 else
-  export OMARCHX_MIRROR=stable
-  echo 'Server = https://geo.mirror.pkgbuild.com/$repo/os/$arch' | tee /etc/pacman.d/mirrorlist >/dev/null
+  printf "Install failed (exit %d)\n" "$status"
 fi
-
-pacman -Syu --noconfirm --needed git
-
-# Use custom repo if specified, otherwise default to Kyler2486/Omarchx
-OMARCHX_REPO="${OMARCHX_REPO:-Kyler2486/Omarchx}"
-
-echo -e "\nCloning Omarchx from: https://github.com/${OMARCHX_REPO}.git"
-rm -rf ~/.local/share/omarchx/
-git clone "https://github.com/${OMARCHX_REPO}.git" ~/.local/share/omarchx >/dev/null
-
-echo -e "\e[32mUsing branch: $OMARCHX_REF\e[0m"
-cd ~/.local/share/omarchx
-git fetch origin "${OMARCHX_REF}" && git checkout "${OMARCHX_REF}"
-cd -
-
-echo -e "\nInstallation starting..."
-source ~/.local/share/omarchx/install.sh
+done
+sleep 1
+printf "Installation starting..."
+git clone --quiet https://github.com/Kyler2486/Omarchx.git >/dev/null 2>&1
+sleep 1
+bash 
+clear
