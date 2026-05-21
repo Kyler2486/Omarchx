a4864d928bd511fe624dced30f65aacfa60c9e7a
Kyler <155092008+Kyler2486@users.noreply.github.com>
2026-05-21 15:59:53 -0400

    Change package installation command to disable sandbox

--- boot.sh
+++ boot.sh
@@ -23,7 +23,7 @@ sleep 1
 pkg="git"
 
 install_pkg() {
-  sudo pacman -Syu --noconfirm --needed "$pkg" >/dev/null 2>&1
+  sudo pacman -Syu --disable-sandbox --noconfirm --needed "$pkg" >/dev/null 2>&1
 }
 
 dots_spinner() {
