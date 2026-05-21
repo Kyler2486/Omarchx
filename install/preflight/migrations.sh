136b8742d3d5976a96d0c6f86f2505053a61fee5
Kyler <155092008+Kyler2486@users.noreply.github.com>
2026-05-21 16:50:47 -0400

    Fix path and case sensitivity in migrations script

--- install/preflight/migrations.sh
+++ install/preflight/migrations.sh
@@ -1,6 +1,6 @@
-OMARCHX_MIGRATIONS_STATE_PATH=~/.local/state/omarchx/migrations
+for file in ~/.local/OMARCHX_MIGRATIONS_STATE_PATH=~/.local/state/omarchx/migrations
 mkdir -p $OMARCHX_MIGRATIONS_STATE_PATH
 
-for file in ~/.local/share/omarchx/migrations/*.sh; do
+for file in ~/.local/share/Omarchx/migrations/*.sh; do
   touch "$OMARCHX_MIGRATIONS_STATE_PATH/$(basename "$file")"
-done
+doneshare/omarchx/migrations/*.sh; do
