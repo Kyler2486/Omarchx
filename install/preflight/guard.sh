e31f7550b3734c54fc77be6d990e248b6ba4efe3
Kyler <155092008+Kyler2486@users.noreply.github.com>
2026-05-21 15:04:46 -0400

    Update guard.sh to improve error messages

--- install/preflight/guard.sh
+++ install/preflight/guard.sh
@@ -1,9 +1,14 @@
+# Must not be run as root
+if [[ $EUID -eq 0 ]]; then
+  abort "Must not be run as root"
+fi
+
 # Must be an Arch distro
 if [[ ! -f /etc/arch-release ]]; then
   abort "Vanilla Arch"
 fi
 
-# Must not have Gnome or KDE already install
+# Must not have Gnome or KDE already installed
 if pacman -Qe gnome-shell &>/dev/null || pacman -Qe plasma-desktop &>/dev/null; then
   abort "Fresh + Vanilla Arch"
 fi
