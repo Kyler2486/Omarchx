017a01bdce45b0d9250f37f6351b5aab5f3a5114
Kyler <155092008+Kyler2486@users.noreply.github.com>
2026-05-24 15:39:25 -0400

    Update wifi-powersave-rules.sh

--- install/config/wifi-powersave-rules.sh
+++ install/config/wifi-powersave-rules.sh
@@ -1,7 +1,10 @@
+# Hardcode real user home so udev rules work correctly when triggered as root
+REAL_HOME=$(getent passwd "${SUDO_USER:-$USER}" | cut -d: -f6)
+
 if omarchx-battery-present; then
-  cat <<EOF | sudo tee "/etc/udev/rules.d/99-wifi-powersave.rules"
-SUBSYSTEM=="power_supply", ATTR{type}=="Mains", ATTR{online}=="0", RUN+="/usr/bin/systemd-run --no-block --collect --unit=omarchx-wifi-powersave-on $HOME/.local/share/Omarchx/bin/omarchx-wifi-powersave on"
-SUBSYSTEM=="power_supply", ATTR{type}=="Mains", ATTR{online}=="1", RUN+="/usr/bin/systemd-run --no-block --collect --unit=omarchx-wifi-powersave-off $HOME/.local/share/Omarchx/bin/omarchx-wifi-powersave off"
+  cat <<EOF | sudo tee "/etc/udev/rules.d/99-wifi-powersave.rules" >/dev/null
+SUBSYSTEM=="power_supply", ATTR{type}=="Mains", ATTR{online}=="0", RUN+="/usr/bin/systemd-run --no-block --collect --unit=omarchx-wifi-powersave-on $REAL_HOME/.local/share/Omarchx/bin/omarchx-wifi-powersave on"
+SUBSYSTEM=="power_supply", ATTR{type}=="Mains", ATTR{online}=="1", RUN+="/usr/bin/systemd-run --no-block --collect --unit=omarchx-wifi-powersave-off $REAL_HOME/.local/share/Omarchx/bin/omarchx-wifi-powersave off"
 EOF
 
   sudo udevadm control --reload
