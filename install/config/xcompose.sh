0ffc9ae9a25dfec2903280627c3797861fec6b6d
Kyler <155092008+Kyler2486@users.noreply.github.com>
2026-05-24 15:38:03 -0400

    Update xcompose.sh

--- install/config/xcompose.sh
+++ install/config/xcompose.sh
@@ -1,11 +1,17 @@
 # Set default XCompose that is triggered with CapsLock
-tee ~/.XCompose >/dev/null <<EOF
-# Run omarchx-restart-xcompose to apply changes
+{
+    echo "# Run omarchx-restart-xcompose to apply changes"
+    echo ""
+    echo "# Include fast emoji access"
+    echo "include \"%H/.local/share/Omarchx/default/xcompose\""
+    echo ""
+    echo "# Identification"
 
-# Include fast emoji access
-include "%H/.local/share/Omarchx/default/xcompose"
+    if [[ -n "${OMARCHX_USER_NAME//[[:space:]]/}" ]]; then
+        echo "<Multi_key> <space> <n> : \"$OMARCHX_USER_NAME\""
+    fi
 
-# Identification
-<Multi_key> <space> <n> : "$OMARCHX_USER_NAME"
-<Multi_key> <space> <e> : "$OMARCHX_USER_EMAIL"
-EOF
+    if [[ -n "${OMARCHX_USER_EMAIL//[[:space:]]/}" ]]; then
+        echo "<Multi_key> <space> <e> : \"$OMARCHX_USER_EMAIL\""
+    fi
+} | tee ~/.XCompose >/dev/null
